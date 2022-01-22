fc8cee4c85cc590724a6568d6d0bb68d903647fc
Victor-Aleix <araventos11@gmail.com>
2022-01-22 16:52:10 +0100

    messing with BoxLayout

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.util.*;
 
 public class Main {
@@ -15,25 +15,23 @@ public class Main {
         JFrame frame = new JFrame();
         JButton timerButton = new JButton("Start/Stop");
         JLabel timerLabel = new JLabel("Timer stopped"); 
-        JPanel topPanel = new JPanel();
-        JPanel botPanel = new JPanel();
+        JPanel panel = new JPanel();
         Timer timer = new Timer(25,0);
 
-        //botPanel.setLayout(new BorderLayout());
-        //topPanel.setLayout(new BorderLayout());
-        //timerButton.setBounds(100,100,200,100);
-        //timerLabel.setBounds(150,300,500,100);
         frame.setTitle("Pomodoro");
-        //frame.setLayout(new BorderLayout());
-        //botPanel.add(timerLabel, BorderLayout.CENTER);
-        //topPanel.add(timerButton, BorderLayout.CENTER);
-        //frame.add(topPanel, BorderLayout.NORTH);
-        //frame.add(botPanel, BorderLayout.SOUTH);
-        //topPanel.setPreferredSize(new Dimension(100,300));
-        //botPanel.setPreferredSize(new Dimension(100,300));
-        frame.add(timerButton);
-        frame.add(timerLabel);
-        frame.setSize(400,600);
+        frame.setLayout(new FlowLayout());
+
+        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        timerLabel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        timerButton.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        panel.add(timerLabel);
+        panel.add(timerButton);
+
+        frame.add(Box.createHorizontalGlue());
+        frame.add(panel);
+        frame.add(Box.createHorizontalGlue());
+        frame.pack();
         frame.setVisible(true);
 
         timerButton.addActionListener(new ActionListener() {
